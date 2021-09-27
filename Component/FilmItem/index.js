8eaecd6cd35f965144b98e7c262d1830bd67aaa4
Zubiik <elodiefrappat08@gmail.com>
2021-09-27 14:30:09 +0200

    fix: design

--- Component/FilmItem/index.js
+++ Component/FilmItem/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, TextInput, Button, FlatList,StyleSheet,Text,Image } from 'react-native';
-import { Container, OverviewCustom, ImageCustom, HeaderContainer, DateCustom, InfoCustom, OverviewTextCustom } from './styled';
+import { Container, OverviewCustom, TitleTextCustom, DateTextCustom, ImageCustom, HeaderContainer, DateCustom, InfoCustom, VoteTextCustom, OverviewTextCustom } from './styled';
 
 export default function FilmItem({film}) {
 console.log(film);
@@ -11,8 +11,8 @@ console.log(film);
         </ImageCustom>
     <InfoCustom>
     <HeaderContainer>
-        <Text>{film.title}</Text>
-        <Text>{film.vote_average}/10</Text>
+        <TitleTextCustom>{film.title}</TitleTextCustom>
+        <VoteTextCustom>{film.vote_average}/10</VoteTextCustom>
     </HeaderContainer>
     
     <OverviewCustom>
@@ -20,7 +20,7 @@ console.log(film);
     </OverviewCustom>
 
     <DateCustom>
-        <Text>Sorti le {film.release_date}</Text>
+        <DateTextCustom >Sorti le {film.release_date}</DateTextCustom>
     </DateCustom>
     </InfoCustom>
 
